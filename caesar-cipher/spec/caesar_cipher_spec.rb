02ae6b17ec1c4b25cc6ea79a48925ec0cbdc21e5
Jacob Sanders <sanders.jms@gmail.com>
2023-04-17 21:12:34 -1000

    finished test for keeping punctuation the same

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -8,4 +8,8 @@ describe "#caesar_cipher" do
   it "wraps from z to a" do
     expect(caesar_cipher("wxyz", 5)).to eql("bcde")
   end
+
+  it "keeps same punctuation used" do
+    expect(caesar_cipher("this is one string; this continues the string.", 5)).to eql("ymnx nx tsj xywnsl; ymnx htsynszjx ymj xywnsl.")
+  end
 end
