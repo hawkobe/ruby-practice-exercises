251455b5b2e66c3f00f004e61f043cb002fb44bf
Jacob Sanders <sanders.jms@gmail.com>
2023-04-17 21:08:15 -1000

    finished second test for wrapping from z to a

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "returns a basic cipher with the given shift" do
     expect(caesar_cipher("this is the string", 5)).to eql("ymnx nx ymj xywnsl")
   end
+
+  it "wraps from z to a" do
+    expect(caesar_cipher("wxyz", 5)).to eql("bcde")
+  end
 end
